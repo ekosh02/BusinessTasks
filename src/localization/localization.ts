712c446628878675dba7679f1f94390dc19d08fc
turapbaeveldos <turapbaeveldos@gmail.com>
2024-05-22 12:57:23 +0500

    added localization

---
+++ src/localization/localization.ts
@@ -0,0 +1,11 @@
+import LocalizedStrings from 'react-native-localization';
+
+export const strings = new LocalizedStrings({
+  ru: {
+    'Введите логин': 'Введите логин',
+    'Введите пароль': 'Введите пароль',
+    'Введите имя': 'Введите имя',
+    'Введите фамилию': 'Введите фамилию',
+    'Авторизоваться': 'Авторизоваться',
+  },
+});
